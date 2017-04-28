0b751896aaf547deca43ac1de4069c5934d802cb
Robin Weiss <github@robinweiss.de>
2017-04-28 19:51:15 +0200

    Add dynamic greeting

--- Implementation/Data Visualisation/Controllers/HomeController.cs
+++ Implementation/Data Visualisation/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Data_Visualisation.Controllers
 {
@@ -6,9 +7,13 @@ namespace Data_Visualisation.Controllers
     {
         public IActionResult Index()
         {
+            int hour = DateTime.Now.Hour;
+            ViewBag.greeting = hour < 10 ? "Guten Morgen!" : "Hallo!";
+            ViewBag.lastUpdated = DateTime.Now;
+
             return View();
         }
-        
+
         public IActionResult Error()
         {
             return View();
