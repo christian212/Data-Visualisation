ce2166e0ce7a4ab016547cea01ae36cd10112725
Rob Weiss <github@robinweiss.de>
2017-06-22 09:05:26 +0200

    Close navbar on scroll and outside-click

--- Implementation/App/Client/app/components/dropdown-split/dropdown-split.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
- 
-@Component({
-  selector: 'dropdown-split',
-  templateUrl: './dropdown-split.component.html'
-})
-export class DropdownSplitComponent {
- 
-}
--- Implementation/App/Client/app/components/navbar/navbar.component.ts
+++ Implementation/App/Client/app/components/navbar/navbar.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 
 @Component({
-    selector: 'nav-bar',
-    templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.css']
+  selector: 'nav-bar',
+  templateUrl: './navbar.component.html'
 })
 export class NavBarComponent {
-  show:boolean = false;
+  show: boolean = false;
+
+  constructor(private _eref: ElementRef) { }
+
+  @HostListener('document:click', ['$event'])
+  onClick(event) {
+    if (!this._eref.nativeElement.contains(event.target))
+      this.show = false;
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event) {
+    this.show = false;
+  }
+
   toggleCollapse() {
     this.show = !this.show
   }
